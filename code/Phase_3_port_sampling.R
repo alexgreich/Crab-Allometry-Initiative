ddc2debc12f0075fa8390ccf39c750028a61fc31
Alex Reich <alex.reich@alaska.gov>
2025-12-01 08:33:30 -0900

    a wranglin'

--- code/Phase_3_port_sampling.R
+++ code/Phase_3_port_sampling.R
@@ -12,7 +12,8 @@
 library(dplyr)
 library(tidyverse)
 library(cowplot)
-library(adfgcolors)
+#library(adfgcolors)
+library(PNWColors)
 library(adfggraph) #why not...
 
 #read in data
@@ -34,16 +35,35 @@ View(port_dat1)
 #restructure dataframe so that I indentify true postives, false postives, true negatives, false negatives for 31 and 32
 names(port_dat1)
 
+#port_dat1_31 <- port_dat1 %>% select
+
 port_dat_31_results <- port_dat1 %>%
   mutate(false_positive = if_else(Stick.legal == 1 & Legal...31mm == 2, 1, 0), #it was legal, and the coxa is wrong (BAD!!)
-         true_positive = if_else(Stick.legal == 1 & Legal...31mm == 1, 1, 0), #it was legal, coxa says it's legal too
-         false_negative = if_else(Stick.legal == 2 & Legal...31mm == 1, 1, 0), #they got away with it (not as bad as false positive)
-         true_negative = if_else(Stick.legal == 2 & Legal...31mm == 2, 1, 0)
-         ) #next select for just the columns you need AGR here!!
+         true_positive = if_else(Stick.legal == 1 & Legal...31mm == 1, 1, 0)#, #it was legal, coxa says it's legal too
+        # false_negative = if_else(Stick.legal == 2 & Legal...31mm == 1, 1, 0)#, #they got away with it (not as bad as false positive)
+        # true_negative = if_else(Stick.legal == 2 & Legal...31mm == 2, 1, 0)
+         ) %>% #next select for just the columns you need AGR here!!
+  select(-Legal..32mm) %>%
+  rename(Coxa.legal = Legal...31mm) %>%
+  mutate(Test.threshold = "31 mm")
 
 port_dat_32_results <- port_dat1 %>%
   mutate(false_positive = if_else(Stick.legal == 1 & Legal..32mm == 2, 1, 0), #it was legal, and the coxa is wrong (BAD!!)
-         true_positive = if_else(Stick.legal == 1 & Legal..32mm == 1, 1, 0), #it was legal, coxa says it's legal too
-         false_negative = if_else(Stick.legal == 2 & Legal..32mm == 1, 1, 0), #they got away with it (not as bad as false positive)
-         true_negative = if_else(Stick.legal == 2 & Legal..32mm == 2, 1, 0)
-  ) 
+         true_positive = if_else(Stick.legal == 1 & Legal..32mm == 1, 1, 0)#, #it was legal, coxa says it's legal too
+         #false_negative = if_else(Stick.legal == 2 & Legal..32mm == 1, 1, 0), #they got away with it (not as bad as false positive)
+         #true_negative = if_else(Stick.legal == 2 & Legal..32mm == 2, 1, 0)
+  ) %>%
+  select(-Legal...31mm) %>%
+  rename(Coxa.legal = Legal..32mm) %>%
+  mutate(Test.threshold = "32 mm")
+
+#next, combine these
+dat_comb <- rbind(port_dat_31_results, port_dat_32_results)
+#AGR HERE!!
+
+#this was really only a test for false positive vs. true positive...
+pnw_palettes$Bay #note the palette color ID #'s
+
+#combine the 31 and 32mm results into one df please.
+
+
